d3313e3afc2d18088c538fff575bc3ba6bd3ca0b
Ghong <ekzmqaz@naver.com>
2020-09-10 22:48:27 +0900

    validate update

--- public/js/validate.js
+++ public/js/validate.js
@@ -1,5 +1,9 @@
 $(function(){
-    let flag = true;
+    let flag1 = false;
+    let flag2 = false;
+    let flag3 = false;
+    let flag4 = false;
+    let flag5 = false;
     $(".checkId").on({"blur":function(){
         const p_id = $("#p_id").val();
             $.ajax({
@@ -9,13 +13,13 @@ $(function(){
                 success:(data)=>{
                     if(data=='0'){
                         $("#noId").css({"color":"red"}).text("이미 존재하는 아이디입니다.");
-                        flag = false;
+                        flag1 = false;
                     }else if(data=='1'){
                         $("#noId").css({"color":"red"}).text("6자 이상, 숫자와 영문자만 됩니다.");
-                        flag = false;
+                        flag1 = false;
                     }else{
                         $("#noId").css({"color":"green"}).text("어썸하네여! ㅎ;");
-                        flag = true;
+                        flag1 = true;
                     }
                 },error:(error)=>{
                     console.error(error);
@@ -23,7 +27,7 @@ $(function(){
         })
     },"focus":function(){
         $("#noId").text("");
-            flag = true;
+            flag2 = true;
     }
     })
     $(".checkPass").on({
@@ -31,15 +35,15 @@ $(function(){
         let password = $("#password").val();
         if(!/^[a-zA-Z0-9]{8,16}$/.test(password)){
             $("#noPass").css({"color":"red"}).text("8-16자 숫자와 영문자");
-            flag = false;
+            flag2 = false;
         }else{
             $("#noPass").css({"color":"green"}).text("사용 가능한 비밀번호 입니다 ~.~");
-            flag = true;
+            flag2 = true;
         }
     },
         "focus":function(){
             $("#noPass").text("");
-            flag = true;
+            flag2 = true;
 }})
     $(".checkRePass").on({
         "blur":function(){
@@ -47,27 +51,27 @@ $(function(){
         let rePass = $("#rePass").val();
         if(password != rePass){
             $("#noRePass").text("비밀번호를 확인해주셈");
-            flag = false;
+            flag3 = false;
         }
     },
         "focus":function(){
             $("#noRePass").text("");
-            flag = true;
+            flag3 = true;
 }})
     $(".checkPhone").on({
         "blur":function(){
             let ph_no = $("#ph_no").val();
             if(!/^[0-9]{8,}$/.test(ph_no)){
                 $("#noPhone").css({"color":"red"}).text("올바른 전화번호를 입력하세요.");
-                flag = false;
+                flag4 = false;
             }else{
                 $("#noPhone").css({"color":"green"}).text("당신의 번호는 이제 제겁니다. zㅅz;");
-                flag = true;
+                flag4 = true;
         }
     },
     "focus":function(){
         $("#noPhone").text("");
-        flag = true;
+        flag4 = true;
     }
 })
     $(".checkEmail").on({
@@ -75,19 +79,19 @@ $(function(){
             let email = $("#email").val();
             if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)){
                 $("#noEmail").css({"color":"red"}).text("올바른 이메일을 입력해주세요.");
-                flag = false;
+                flag5 =  false;
             }else{
                 $("#noEmail").css({"color":"green"}).text("사용 가능한 이메일입니다. ㅎㅅㅎ;");
-                flag = true;
+                flag5 = true;
             }
         },
         "focus":function(){
             $("#noEmail").text("");
-            flag = true;
+            flag5 = true;
         }
 })
     $("#canSubmit").click(function(e){
-        if(!flag){
+        if(!(flag1&&flag2&&flag3&&flag4&&flag5)){
             alert('메시지를 확인해주세요 ㅎㅎ;');
             e.preventDefault();
         }
