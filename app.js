88b9917317fa192f0652757f877b39611caaa5ff
Yoon Heesun <yoonheesun@Yoons-MacBook-Air.local>
2020-09-10 23:21:41 +0900

    910 first

--- app.js
+++ app.js
@@ -1,71 +1,78 @@
-const express = require('express');
-const dotenv = require('dotenv'); 
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const port = process.env.PORT || 5000 ;
-const path = require('path');
-const indexRouter = require('./routes');
-const authRouter = require('./routes/auth');
-const mypageRouter = require('./routes/mypage');
-const passport = require('passport');
-const connect = require('./models');
-const flash = require('connect-flash');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const port = process.env.PORT || 5000;
+const path = require("path");
+const indexRouter = require("./routes");
+const authRouter = require("./routes/auth");
+const mypageRouter = require("./routes/mypage");
+const passport = require("passport");
+const connect = require("./models");
+const flash = require("connect-flash");
 
 dotenv.config();
 
 //passport폴더 안에 정의된 함수들 임포트, 해주어야함 !!
-const passportConfig = require('./passport');
+const passportConfig = require("./passport");
 passportConfig();
 
 const app = express();
 //view 엔진을 html(ejs)로 설정
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 //configure 함수의 첫번째 인자로 폴더 경로 설정
 //몽구스를 이용한 몽고디비 연결
 connect();
 
 //미들 웨어 정리
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  resave:false,
-  saveUninitialized:false,
-  secret:process.env.COOKIE_SECRET,
-  cookie:{
-      httpOnly:true,
-      secure:false,
-  },
-}));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
 app.use(flash());
 //로그인 인증
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use((req,res,next) =>{
+app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
 });
 
 //라우팅
-app.use('/',indexRouter);
-app.use('/auth',authRouter);
-app.use('/mypage',mypageRouter);
-
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
+app.use("/mypage", mypageRouter);
+app.get("/aboutus", function (req, res) {
+  res.render(aboutus);
+});
+app.get("/healthtopic", function (req, res) {
+  res.render(healthtopic);
+});
 
 //에러 처리
 app.use((req, res, next) => {
-    const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
-  });
-  app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
-  });
-app.listen(port, ()=> console.log(`Listening on port ${port}`));
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Patient = require('../models/patients');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares/middlewares');
- 
+const Patient = require("../models/patients");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares/middlewares");
+
 //api를 통해 환자 정보를 모두 가져옴
-router.get('/api/patients', (req,res)=>{
-    Patient.findAll()
-    .then(result=>{
-        res.send(result);
+router.get("/api/patients", (req, res) => {
+  Patient.findAll()
+    .then((result) => {
+      res.send(result);
     })
-    .catch(err =>{
-        console.error(err);
-    })
-})
+    .catch((err) => {
+      console.error(err);
+    });
+});
 //메인화면을 렌더링하는 함수
-router.get('/',(req,res,next)=>{
-    res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 //회원가입으로 들어오는 경로 처리
-router.get('/join',isNotLoggedIn,(req,res,next)=>{
-    const message = req.flash('message');
-    const noPass = req.flash('noPass');
-    const rePass = req.flash('rePass');
-    const noEmail = req.flash('noEmail');
-    res.render('join',{message:message,
-    noPass:noPass,
-    rePass:rePass,
-    noEmail:noEmail});
-})
+router.get("/join", isNotLoggedIn, (req, res, next) => {
+  const message = req.flash("message");
+  const noPass = req.flash("noPass");
+  const rePass = req.flash("rePass");
+  //const noEmail = req.flash("noEmail");
+  res.render("join", {
+    message: message,
+    noPass: noPass,
+    rePass: rePass,
+    //noEmail: noEmail,
+  });
+});
 
-router.get('/login',isNotLoggedIn,(req,res,next)=>{
-    const message = req.flash('message');
-    res.render('login',{message:message});
+router.get("/login", isNotLoggedIn, (req, res, next) => {
+  const message = req.flash("message");
+  res.render("login", { message: message });
 });
-router.get('/aboutus',(req,res,next)=>{
-    res.render('aboutus');
+router.get("/aboutus", (req, res, next) => {
+  res.render("aboutus");
 });
-router.get('/healthtopic',(req,res,next)=>{
-    res.render('healthtopic');
+router.get("/healthtopic", (req, res, next) => {
+  res.render("healthtopic");
 });
 
 module.exports = router;
