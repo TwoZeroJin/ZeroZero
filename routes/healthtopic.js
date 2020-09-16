f679dbd3ae7f5dd7e5080cd1fd44d3faad6ec1bb
hyunhii <be_hyunny@naver.com>
2020-09-16 14:05:41 +0900

    simplify code

--- routes/healthtopic.js
+++ routes/healthtopic.js
@@ -8,36 +8,35 @@ const healthInfo = "http://www.cdc.go.kr/gallery.es?mid=a20509000000&bid=0007";
 router.get('/', function(req, res, next) {
     axios.get(coronaUrl).then(html => {
         axios.get(healthInfo).then(html2 => {
-            /* 질병관리청 이달의 건강소식 */
-            const infoArr = [];
-            let $ = cheerio.load(html2.data);
-            const infoTag = $("div.galleryList ul li");
-            infoTag.each(function(i, elem) {
-                let infoObj = {
-                    _title : $(this).find("a").attr("title"),
-                    _addr : $(this).find("a").attr("href")
-                }
-                infoArr.push(infoObj);
-            })
-            console.log(infoArr);
-
-
             /* 코로나 확진자 수 크롤링 */
-            const resultArr = [];
-            $ = cheerio.load(html.data);
-            const parentTag = $("div.liveNum ul.liveNum li");
+            const coronaArr = [];
+            let $ = cheerio.load(html.data);
+            const coronaTag = $("div.liveNum ul.liveNum li");
 
-            parentTag.each(function(i, elem){
-                let itemObj = { 
+            coronaTag.each(function(i, elem){
+                let coronaObj = { 
                     _text : $(this).find("strong").text(), 
                     _num :$(this).find("span").text()
                 }
-                resultArr.push(itemObj);
+                coronaArr.push(coronaObj);
             });
 
-            console.log(resultArr);
+            console.log(coronaArr);
 
-            res.render('healthtopic',{ infoArr : infoArr, resultArr:resultArr});
+             /* 질병관리청 이달의 건강소식 */
+             const infoArr = [];
+             $ = cheerio.load(html2.data);
+             const infoTag = $("div.galleryList ul li");
+             infoTag.each(function(i, elem) {
+                 let infoObj = {
+                     _title : $(this).find("a").attr("title"),
+                     _addr : $(this).find("a").attr("href")
+                 }
+                 infoArr.push(infoObj);
+             })
+             console.log(infoArr);
+
+            res.render('healthtopic',{ infoArr : infoArr, coronaArr:coronaArr});
         });
     });
 });
