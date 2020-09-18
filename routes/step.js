11192a34bca4429f681007f02292270953171ed9
Ghong <ekzmqaz@naver.com>
2020-09-18 18:28:02 +0900

    test

--- routes/step.js
+++ routes/step.js
@@ -6,8 +6,8 @@ const { isLoggedIn, isNotLoggedIn } = require('./middlewares/middlewares');
 
 /* -----------------STEP 1----------------- */
 /* 예진표 STEP1 - get 메소드 페이지 */
-router.get('/step1', isLoggedIn, function(req, res, next) {
-    Step1.findOne({p_id : res.locals.user}, function(err, step1) {  // 로그인 되어있는 사용자의 정보를 'step1' 컬렉션에서 찾아
+router.get('/step1', isLoggedIn, async(req, res, next)=>{
+    await Step1.findOne({p_id : res.locals.user}, function(err, step1) {  // 로그인 되어있는 사용자의 정보를 'step1' 컬렉션에서 찾아
         res.render('question/step1', {step1: step1});              // 뷰로 보내기
     });
 });
@@ -22,8 +22,8 @@ router.post('/step1', function(req, res) {
 });
 
 /* 예진표 STEP1 - (재작성시) post 메소드 페이지 */
-router.put('/step1_update', function(req, res) {
-    Step1.findOneAndUpdate({p_id : res.locals.user}, req.body, function(err, step1) {       // 로그인 되어있는 사용자의 정보를 컬렉션에서 찾아 수정(업데이트)하기
+router.put('/step1_update', async(req, res)=>{
+    await Step1.findOneAndUpdate({p_id : res.locals.user}, req.body, function(err, step1) {       // 로그인 되어있는 사용자의 정보를 컬렉션에서 찾아 수정(업데이트)하기
         if(err) return res.json(err);
         res.redirect('/question/step2');        // step2 라우터로 이동
     });
@@ -36,8 +36,8 @@ router.get('/step2', isLoggedIn, function(req, res, next) {
 });
 
 /* 예진표 STEP2 - post 메소드 페이지 */
-router.post('/step2',isLoggedIn,function(req, res) {
-    Step2.create(req.body, function(err, step2) {       // step2.ejs의 form태그에 적은 모든 내용들을 step2 컬렉션에 저장
+router.post('/step2',isLoggedIn,async(req, res)=>{
+    await Step2.create(req.body, function(err, step2) {       // step2.ejs의 form태그에 적은 모든 내용들을 step2 컬렉션에 저장
         console.log('STEP 2의 db저장완료');
         res.redirect('/question/step3');
     });
