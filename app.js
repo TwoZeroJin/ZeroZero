8cb286d84e176e8c9809a47d7ad923d43eabcf97
Ghong <ekzmqaz@naver.com>
2020-09-18 18:17:30 +0900

    test

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ connect();
 if (process.env.NODE_ENV === "production") {
   app.use(morgan("combined"));
   // app.use(helmet());
-  // app.use(hpp());
+  app.use(hpp());
 } else {
   app.use(morgan("dev"));
 }
