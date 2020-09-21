69886c33b23d8d656b05b8b23c6964c7681843ec
KimDavinchi <kdbdml@gmail.com>
2020-09-21 20:25:53 +0900

    comment, index.ejs modified

--- models/qna.js
+++ models/qna.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const Counter = require("./counter");
-const autoIncrement = require('mongoose-auto-increment');
-autoIncrement.initialize(mongoose);
 
 //schema
 const { Schema } = mongoose;
@@ -37,14 +35,6 @@ const qnaSchema = new Schema({
   updatedAt: { type: Date },
 });
 
-qnaSchema.plugin(autoIncrement.plugin, {
-  model:'qna',
-  field: 'numId', // auto-increment할 field
-  startAt: 1, // 0에서 부터
-  increment: 1 // 1씩 증가
- });
-
-
 qnaSchema.pre("save", async function (next) {
   let qna = this;
   if (qna.isNew) {
@@ -52,7 +42,7 @@ qnaSchema.pre("save", async function (next) {
     if (!counter) counter = await Counter.create({ name: "qnas" });
     counter.count++;
     counter.save();
-    //qna.numId = counter.count;
+    qna.numId = counter.count;
   }
   return next();
 });
